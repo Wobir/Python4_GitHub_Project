58cfdaf299a16c3db7b83546e506125ff8133411
Wobir <ilyakorobi4un@yandex.ru>
2026-04-27 19:45:03 +0500

    простая бд через sqlite3

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, session
-from models import db, Users, Posts, Messages
+from models import *
 from forms import RegForm, AuthForm, CreatePostForm
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime, timedelta
@@ -13,13 +13,15 @@ def base():
 
 @app.route("/reg/", methods =["GET", "POST"])
 def reg():
+    
     reg_form = RegForm()
     if reg_form.validate_on_submit():
         if request.method == "POST":
+            check_db()
             name = request.form.get("name")
             password = request.form.get("password")
             email = request.form.get("email")
-            if Users.query.filter_by(name = name).first() or Users.query.filter_by(email = email).first():
+            if find_user_by_name(name) or find_user_by_email(email): #Users.query.filter_by(name = name).first() or Users.query.filter_by(email = email).first():
                 message = "Пользователь с таким именем и почтой уже зарегестрирован"
                 return render_template("reg.html", form = reg_form, message = message)
             new_user = Users(
@@ -27,21 +29,22 @@ def reg():
                 email = email,
                 password = password
             )
-
-            db.session.add(new_user)
-            db.session.commit()
+            create_user(new_user)
     return render_template("reg.html", form = reg_form)
 
 @app.route("/login/", methods = ["POST", "GET"])
 def auth():
+    if "user_name" in session:
+        print("user_name" in session)
+        return redirect(url_for("profile"))
     auth_form = AuthForm()
     if auth_form.validate_on_submit():
         if request.method=="POST":
             name=request.form.get("name")
             password= request.form.get("password")
-            user = Users.query.filter_by(name = name ).first()
+            user = find_user_by_name(name)
             if not user:
-                user = Users.query.filter_by(email= name).first()
+                user = find_user_by_email(name)
             if user and user.password == password:
                 session["user_id"] = user.id
                 session["user_name"] = user.name
@@ -51,13 +54,15 @@ def auth():
                 return render_template("auth.html", form = auth_form, error = error)
     return render_template ("auth.html", form = auth_form)
 
+@app.route("/profile/")
+def profile():
+    user = find_user_by_name(session["user_name"])
+    return render_template("profile.html", user = user)
+
 
 @app.route('/')
-def main():
+def index():
     return render_template('index.html')
-
-
-
 
 app.run(debug=True)
 
--- models.py
+++ models.py
@@ -1,16 +1,81 @@
-from flask_sqlalchemy import SQLAlchemy
+import sqlite3 as sql
 
-db = SQLAlchemy()
+def connect_to_db():
+    return sql.connect("instance/db.db")
 
-class Users(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), unique = True, nullable = False)
-    password = db.Column(db.String(100), nullable = False)
-class Posts(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    user = db.Column(db.Integer(), db.ForeignKey("user.id"))
-    name = db.Column(db.String())
-class Messages(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    user = db.Column(db.Integer, db.ForeignKey("user.id"))
-    text = db.Column(db.Text, nullable=False)
+def check_db():
+    db = connect_to_db()
+    cur = db.cursor()
+    cur.execute(
+        "SELECT name FROM sqlite_master WHERE type='table' AND name='users';",
+    )
+    if cur.fetchone() is None:
+        init_db()
+    db.close()
+
+def init_db():
+    db = connect_to_db()
+    cur = db.cursor()
+    cur.execute("""
+                CREATE TABLE "users" (
+                "id"	INTEGER NOT NULL,
+                "name"	TEXT NOT NULL UNIQUE,
+                "password_hashed"	TEXT NOT NULL,
+                "email"	TEXT NOT NULL UNIQUE,
+                PRIMARY KEY("id" AUTOINCREMENT)
+                );""")
+    cur.execute("""
+                CREATE TABLE "posts" (
+                    "id"	INTEGER NOT NULL,
+                    "text"	TEXT,
+                    "content"	TEXT,
+                    "date"	INTEGER,
+                    "user_id"	INTEGER,
+                    PRIMARY KEY("id" AUTOINCREMENT),
+                    FOREIGN KEY("user_id") REFERENCES "users"("id")
+                );
+                """)
+class Users():
+    def __init__(self, name, password, email, id = 0):
+        self.id = id
+        self.name = name
+        self.password = password
+        self.email = email
+        
+class Posts():
+    def __init__(self,user_id, id = 0, text = None, content = None, date = None):
+        self.id = id
+        self.user_id = user_id
+        self.text = text
+        self.content = content
+        self.date = date
+        
+def create_user(user: Users):
+    db = connect_to_db()
+    cur = db.cursor()
+    query = f"INSERT INTO users(name, password_hashed, email) VALUES ('{user.name}', '{user.password}', '{user.email}')"
+    cur.execute(query)
+    db.commit()
+    db.close()
+
+def find_user_by_name(name):
+    db = connect_to_db()
+    cur = db.cursor()
+    cur.execute(f"SELECT * FROM users WHERE name='{name}'")
+    result = cur.fetchone()
+    db.close()
+    if result is None or result == False:
+        return False
+    return Users(*result)
+
+def find_user_by_email(email):
+    db = connect_to_db()
+    cur = db.cursor()
+    cur.execute(f"SELECT * FROM users WHERE email='{email}'")
+    result = cur.fetchone()
+    db.close()
+    if result is None or result == False:
+        return False
+
+    return Users(*result)
+    
